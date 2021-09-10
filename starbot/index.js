13b17b2cfd6b44d0a255aa0e689e0d4d5f7a82e2
TFTWPhoenix <61560519+TFTWPhoenix@users.noreply.github.com>
2021-09-10 15:58:41 -0700

    Use git commit for version.

--- starbot/index.js
+++ starbot/index.js
@@ -2,6 +2,9 @@
 const botkit = require('../botkit/index');
 const fs = require('fs');
 
+// Version
+let version = fs.readFileSync(".git/refs/heads/main").toString().substring(0,7);
+
 // Load servers
 let servers = fs.readdirSync("starbot/config/servers");
 for(let i = 0; i < servers.length; i++) {
@@ -13,6 +16,6 @@ for(let i = 0; i < servers.length; i++) {
 function addBot(config) {
     let bot = botkit.createBot(config.host,config.port,config.connectAs);
     bot.on('ready',() => {
-        bot.sendchat("&7Starbot has started. &8(OBR-1.0)")
+        bot.sendchat("&7Starbot has started. &8Version: OBR-" + version);
     })
 }
